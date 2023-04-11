8b6b720efcc79acdd2c1116bb8a3fdf8d29053b1
Paul Wooten <paul.wooten@hyland.com>
2023-04-11 14:59:40 -0400

    refactor: main layout, cleanup test code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { PlayerState } from './state/player.state';
-import { Store } from '@ngrx/store';
-import { setPlayerName } from './actions';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,7 +7,6 @@ import { setPlayerName } from './actions';
 export class AppComponent {
   title = 'ngrx-hangman';
 
-  constructor(private store: Store<PlayerState>) {
-    setTimeout(() => this.store.dispatch(setPlayerName({ playerName: 'dogbiscuit' })), 4000);
+  constructor() {
   }
 }
