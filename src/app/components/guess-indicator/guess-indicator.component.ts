bdf8e8f5e53874ffea8de6dc8e7d4b79e231610c
Paul Wooten <paul.wooten@hyland.com>
2023-04-18 12:41:25 -0400

    feat: display guessedLetterCount of wordLength in guess indicator

--- src/app/components/guess-indicator/guess-indicator.component.ts
+++ src/app/components/guess-indicator/guess-indicator.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { GameService } from '../../services/game.service';
 
 @Component({
@@ -10,10 +10,18 @@ import { GameService } from '../../services/game.service';
 export class GuessIndicatorComponent {
 
   readonly guess$ : Observable<string[]>;
+  readonly guessSummary$: Observable<string>;
+
   readonly isPaused$: Observable<boolean>;
 
   constructor(private gameService: GameService) {
     this.guess$ = this.gameService.getCurrentGuess$();
+    this.guessSummary$ = this.guess$.pipe(map(this.formatGuessSummary));
     this.isPaused$ = this.gameService.getIsPaused$();
+  }
+
+  private formatGuessSummary(guess: string[]) : string {
+    const guessedLetterCount = guess.filter(guessCharacter => guessCharacter != '?').length;
+    return `${guessedLetterCount} of ${guess.length}`;
   }
 }
